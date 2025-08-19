1358e5db32094aca687007ab26c965956e539e50
RAJAT SHARMA <rajat.sharma2727@gmail.com>
2025-08-20 00:12:31 +0530

    Most of the changes are added

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = ({ name }) => (
-  <footer className="footer">
-    <p>© {new Date().getFullYear()} {name}. All rights reserved.</p>
-  </footer>
-);
-
-export default Footer;
